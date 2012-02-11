4050093981f18e84575d3a130b9e59d1be932fa1
Robert Xu <robxu9@gmail.com>
2012-02-11 12:30:52 -0500

    Here we go. Again.
    Signed-off-by: Robert Xu <robxu9@gmail.com>

---
+++ src/main/java/com/cheetahlabs/minequest/BukkitStart.java
@@ -0,0 +1,19 @@
+package com.cheetahlabs.minequest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BukkitStart extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/main/java/com/cheetahlabs/minequest/SpoutStart.java
@@ -0,0 +1,19 @@
+package com.cheetahlabs.minequest;
+
+import org.spout.api.plugin.CommonPlugin;
+
+public class SpoutStart extends CommonPlugin {
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
